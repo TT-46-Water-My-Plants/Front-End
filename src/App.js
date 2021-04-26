b6ede4eb1ed56c99366b8fa0ba955171edb61a93
KrupDat2020 <mark.krupinsky@gmail.com>
2021-04-26 15:34:25 -0500

    code written, on to styling

--- src/App.js
+++ src/App.js
@@ -1,60 +1,8 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import PlantForm from'./PlantForm';
+import React from "react";
+import PlantForm from "./PlantForm";
 
-const initialFormValues = {
-	id: [],
-	nickname: "",
-	species: "",
-	h2ofrequency: "",
+function App() {
+  return <PlantForm />;
 }
 
-export default function App() {
-	const[plants, setPlants] = useState([])
-	const[formValues, setFormValues] = useState(initialFormValues)
-	const updateForm = (inputName, inputValue) => {
-		setFormValues({...formValues,[inputName]: inputValue})
-	}
-
-	const submitForm = () => {
-		const newPlant = {
-			id: formValues.id.trim(),
-			nickname: formValues.nickname.trim(),
-			species: formValues.species.trim(),
-			h2ofrequency: formValues.h2ofrquency.trim(),
-		}
-
-		if(!newPlant.id || !newPlant.nicknamew || !newPlant.species || !newPlant.h2ofrequency) return
-		axios.post('', newPlant)
-		.then(res => {
-			setPlants([...plants, res.data])
-			setFormValues(initialFormValues)
-		})
-		.catch(err => {
-			console.log(err);
-		})
-	}
-
-	useEffect(() => {
-		axios.get('').then(res => setPlants(res.data))
-	}, [])
-
-	return (
-		<div className='container'>
-	        <h1>Testing</h1>
-
-	<PlantForm
-	values={formValues}
-	update={updateForm}
-	submit={submitForm}
-/>
-
-{plants.map(plant => {
-	return (
-		<Plant key={plant.id} details={plant} />
-	)
-})
-}
-</div>
-)
-}
+export default App;
--- src/PlantForm.js
+++ src/PlantForm.js
@@ -1,76 +1,71 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
 
-export default function PlantForm(props) {
-    const { values, update, submit } = props
+const initialFormValues = {
+  id: "",
+  nickname: "",
+  species: "",
+  h2ofrequency: "",
+};
 
-    const onChange = evt => {
-        const { name, value } = evt.target
-        update(name, value)
-    }
+function PlantForm() {
+  const [values, setValues] = useState(initialFormValues);
 
-    const onSubmit = evt => {
-        evt.preventDefault()
-        submit()
-    }
+  const onChange = (evt) => {
+    setValues({ ...values, [evt.target.name]: evt.target.value });
+  };
 
-    return (
-        <form className='form-container' onSubmit={onSubmit}>
-            <div className='form-group inputs'>
-                <label>Id
-                        <input
-                        type="int"
-                        value={values.id}
-                        onChange={onChange}
-                        name=""
-                        />
-                </label>
+  return (
+    <form className="form-container">
+      <div className="form-group inputs">
+        <label>
+          Id
+          <input type="text" value={values.id} onChange={onChange} name="id" />
+        </label>
 
-                <label>Nickname
-                    <input
-                    type="text"
-                    value={values.nickname}
-                    onChange={onchange}
-                    name="nickname"
-                    placeholder="type a nickname here"
-                    />
-                    </label>
+        <label>
+          Nickname
+          <input
+            type="text"
+            value={values.nickname}
+            onChange={onChange}
+            name="nickname"
+            placeholder="type a nickname here"
+          />
+        </label>
 
-                    <label>Species
-                        <input
-                        type="text"
-                        value={values.species}
-                        onChange={onChange}
-                       name="species"
-                       placeholder="input species here"
-                       />
-                    </label>
+        <label>
+          Species
+          <input
+            type="text"
+            value={values.species}
+            onChange={onChange}
+            name="species"
+            placeholder="input species here"
+          />
+        </label>
 
-                       <label>H20 Frequency
-                       <input
-                       type="text"
-                       value={values.h2ofrequency}
-                       onChange={onChange}
-                       name="h2ofrequency"
-                       placeholder="input frequency here"
-                       />
-                       </label>
+        <label>
+          H20 Frequency
+          <input
+            type="text"
+            value={values.h2ofrequency}
+            onChange={onChange}
+            name="h2ofrequency"
+            placeholder="input frequency here"
+          />
+        </label>
 
-                       <div className='submit'>
-                           <button >Submit</button>
-                       </div>
-
-                    
-            </div>
-        </form>
-    )
+        <div className="submit">
+          <button>Submit</button>
+        </div>
+      </div>
+    </form>
+  );
 }
 
-const Container = styled.div`
+const Container = styled.div``;
 
-`
+const Form = styled.form``;
 
-const Form = styled.form`
-
-`
-
+export default PlantForm;
