ecb846f103d8845b36059568935a96decde6d619
g3or3 <>
2021-04-26 17:00:58 -0400

    created store and reducers for auth login/register and plant actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { PrivateRoute } from "./PrivateRoute";
 
 function App() {
-	return <h1>Testing</h1>;
+	return (
+		<Switch>
+			<Route path="/">
+				<div>
+					<h1>testing</h1>
+				</div>
+			</Route>
+		</Switch>
+	);
 }
 
 export default App;
