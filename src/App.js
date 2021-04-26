6b59b12d47aa03cdd7e8f34b4022f955e5836fcc
KrupDat2020 <mark.krupinsky@gmail.com>
2021-04-26 14:29:35 -0500

    app.js finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
+import axios from "axios";
 import React from "react";
+import PlantForm from'./PlantForm';
 
-function App() {
-	return <h1>Testing</h1>;
+const initialFormValues = {
+	id: [],
+	nickname: "",
+	species: "",
+	h2ofrequency: "",
 }
 
-export default App;
+export default function App() {
+	const[plants, setPlants] = useState([])
+	const[formValues, setFormValues] = useState(initialFormValues)
+	const updateForm = (inputName, inputValue) => {
+		setFormValues({...formValues,[inputName]: inputValue})
+	}
+
+	const submitForm = () => {
+		const newPlant = {
+			id: formValues.id.trim(),
+			nickname: formValues.nickname.trim(),
+			species: formValues.species.trim(),
+			h2ofrequency: formValues.h2ofrquency.trim(),
+		}
+
+		if(!newPlant.id || !newPlant.nicknamew || !newPlant.species || !newPlant.h2ofrequency) return
+		axios.post('', newPlant)
+		.then(res => {
+			setPlants([...plants, res.data])
+			setFormValues(initialFormValues)
+		})
+		.catch(err => {
+			console.log(err);
+		})
+	}
+
+	useEffect(() => {
+		axios.get('').then(res => setPlants(res.data))
+	}, [])
+
+	return (
+		<div className='container'>
+	        <h1>Testing</h1>
+
+	<PlantForm
+	values={formValues}
+	update={updateForm}
+	submit={submitForm}
+/>
+
+{plants.map(plant => {
+	return (
+		<Plant key={plant.id} details={plant} />
+	)
+})
+}
+</div>
+)
+}
---
+++ src/PlantForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function PlantForm(props) {
+    const {} = props
+
+    
+}
+
+const Container = styled.div`
+
+`
+
+const Form = styled.form`
+
+`
+
