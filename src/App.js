fa794dd253d225672b14ced3db21c153765fd05e
g3or3 <>
2021-04-26 19:05:57 -0400

    changes to master

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { PrivateRoute } from "./PrivateRoute";
 
+import PlantForm from "./PlantForm";
+import RenderPlants from "./Renderplants";
+
 function App() {
 	return (
 		<Switch>
+			{/* <PrivateRoute path="/dashboard" component={RenderPlants} /> */}
+			<Route path="/dashboard" component={RenderPlants} />
+			<Route path="/newplant" component={PlantForm} />
 			<Route
 				path="/"
 				component={() => {
