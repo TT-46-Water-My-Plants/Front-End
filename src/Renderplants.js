dc2f7a31a05696f8525f59929412d51f73330b8f
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-26 15:18:55 -0600

    added some styling to renderplants

--- src/Renderplants.js
+++ src/Renderplants.js
@@ -7,20 +7,26 @@ const RenderBox=Styled.div`
     flex-direction: row;
     justify-content: space-around;
     background-color: #49FCD4;
-    width: 45%;
+    width: 40%;
     margin: auto;
 `;
+const Info=Styled.div`
+    color: #454848;
+    font-weight: 4.5rem;
+`;
+
 function RenderPlants() {
 
 
     return (
         <RenderBox>
-            <div>
+            <Info>
+                <img src='' alt='Plant'></img>
                 <p>Name: </p>
                 <p>Nickname: </p>
                 <p>Species: </p>
                 <p>Watering Frequency: </p>
-            </div>
+            </Info>
         </RenderBox>
     );
 }
